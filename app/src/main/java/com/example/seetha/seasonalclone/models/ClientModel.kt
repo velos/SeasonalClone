7f2a6a6c52fd665096d986ce94218b3b5fb9b5f2
seethaa <seetha.annamraju@gmail.com>
2018-05-27 17:51:10 -0700

    task 2 - load California data for June

--- app/src/main/java/com/example/seetha/seasonalclone/models/ClientModel.kt
+++ app/src/main/java/com/example/seetha/seasonalclone/models/ClientModel.kt
@@ -1,8 +1,30 @@
 package com.example.seetha.seasonalclone.models
 
+import com.google.gson.annotations.SerializedName
+
 data class ProduceItem(
-        val name: String,
-        val type: String,
-        val description: String
+        @SerializedName("ID") val id: String,
+        @SerializedName("Name") val name: String,
+        @SerializedName("Type") val type: String,
+        @SerializedName("Description") val description: String
 )
 
+data class ProduceByState(
+        @SerializedName("ID") val id: String,
+        @SerializedName("months") val months: ProduceMonth
+)
+
+data class ProduceMonth(
+        @SerializedName("January") val january: Boolean,
+        @SerializedName("February") val february: Boolean,
+        @SerializedName("March") val march: Boolean,
+        @SerializedName("April") val april: Boolean,
+        @SerializedName("May") val may: Boolean,
+        @SerializedName("June") val june: Boolean,
+        @SerializedName("July") val july: Boolean,
+        @SerializedName("August") val august: Boolean,
+        @SerializedName("September") val september: Boolean,
+        @SerializedName("October") val october: Boolean,
+        @SerializedName("November") val november: Boolean,
+        @SerializedName("December") val december: Boolean
+)
