b9a7133c79204eafaa45a63df53235d2c02958f8
seethaa <seetha.annamraju@gmail.com>
2018-05-28 19:13:14 -0700

    task 4/ group fruits and veggies by sections

--- app/src/main/java/com/example/seetha/seasonalclone/adapters/SeasonalAdapter.kt
+++ app/src/main/java/com/example/seetha/seasonalclone/adapters/SeasonalAdapter.kt
@@ -1,33 +1,92 @@
 package com.example.seetha.seasonalclone.adapters
 
 import android.support.v7.widget.RecyclerView
-import android.view.View
 import android.view.ViewGroup
 import com.example.seetha.seasonalclone.models.ProduceItem
 import views.BrowseItemView
+import views.HeaderItemView
 
-class SeasonalAdapter(private var items: List<ProduceItem>) : RecyclerView.Adapter<SeasonalAdapter.ViewHolder>() {
+class SeasonalAdapter(private var items: List<ProduceItem>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+
+    val FRUIT_TYPE = "Fruit"
+    val VEGETABLE_TYPE = "Vegetable"
+    val HERB_TYPE = "Herb"
+    val LEGUME_TYPE = "Legume"
+    val NUT_TYPE = "Nut"
+
+    private val itemsWithHeaders = sortItemsByHeader(items)
 
     init {
         notifyDataSetChanged()
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val itemView = BrowseItemView.inflate(parent)
-        return ViewHolder(itemView)
+    private fun sortItemsByHeader(items: List<ProduceItem>): List<Any> {
+
+        val displayItems = items.filter { item -> item.display }
+        val sortedProduce = mutableListOf<Any>()
+        if (displayItems.isNotEmpty()) {
+            sortedProduce.add(0, "Fruits")
+            sortedProduce.addAll(displayItems.filter { displayItem -> displayItem.type == FRUIT_TYPE })
+        }
+        if (items.isNotEmpty()) {
+            sortedProduce.add("Veggies")
+            sortedProduce.addAll(displayItems.filter { displayItem ->
+                listOf(VEGETABLE_TYPE, HERB_TYPE, LEGUME_TYPE, NUT_TYPE).contains(displayItem.type)
+            })
+        }
+
+        return sortedProduce
+
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.browseItemView.produce = items[position]
+    companion object {
+        private const val VIEW_TYPE_HEADER = 0
+        private const val VIEW_TYPE_ITEM = 1
+    }
+
+
+    override fun getItemViewType(position: Int): Int {
+        if (itemsWithHeaders[position] is ProduceItem) {
+            return VIEW_TYPE_ITEM
+        }
+
+        return VIEW_TYPE_HEADER
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+
+        return when (viewType) {
+            VIEW_TYPE_ITEM -> {
+                val view = BrowseItemView.inflate(parent)
+                SimpleViewHolder(view)
+            }
+            else -> {
+                val headerView = HeaderItemView.inflate(parent)
+                SimpleViewHolder(headerView)
+            }
+        }
+
 
     }
 
-    override fun getItemCount(): Int {
-        return items.size
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder.itemViewType) {
+            VIEW_TYPE_ITEM -> {
+                val itemHolder = holder as SimpleViewHolder<BrowseItemView>
+                itemHolder.view.produce = itemsWithHeaders[position] as ProduceItem
+            }
+            VIEW_TYPE_HEADER -> {
+                val headerHolder = holder as SimpleViewHolder<HeaderItemView>
+                headerHolder.view.header = itemsWithHeaders[position].toString()
+            }
+
+        }
     }
 
-    inner class ViewHolder(v: View) : RecyclerView.ViewHolder(v) {
-        internal val browseItemView: BrowseItemView = v as BrowseItemView
-    }
+    override fun getItemCount(): Int = itemsWithHeaders.size
+
 
 }
