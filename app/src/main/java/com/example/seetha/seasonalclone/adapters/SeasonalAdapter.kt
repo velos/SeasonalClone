718be910631144ed2ba4f6feea7198a037db21ae
seethaa <seetha.annamraju@gmail.com>
2018-06-07 11:40:55 -0700

    solution for task 3 filters

--- app/src/main/java/com/example/seetha/seasonalclone/adapters/SeasonalAdapter.kt
+++ app/src/main/java/com/example/seetha/seasonalclone/adapters/SeasonalAdapter.kt
@@ -1,10 +1,15 @@
 package com.example.seetha.seasonalclone.adapters
 
 import android.support.v7.widget.RecyclerView
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.example.seetha.seasonalclone.R
 import com.example.seetha.seasonalclone.models.ProduceItem
-import views.BrowseItemView
-import views.HeaderItemView
+import com.example.seetha.seasonalclone.util.inflate
+import kotterknife.bindView
 
 class SeasonalAdapter(private val items: List<ProduceItem>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
@@ -25,7 +30,7 @@ class SeasonalAdapter(private val items: List<ProduceItem>) : RecyclerView.Adapt
         val displayItems = items.filter { item -> item.display }
         val sortedProduce = mutableListOf<Any>()
         if (displayItems.isNotEmpty()) {
-            sortedProduce.add( "Fruits")
+            sortedProduce.add("Fruits")
             sortedProduce.addAll(displayItems.filter { displayItem -> displayItem.type == FRUIT_TYPE })
             sortedProduce.add("Veggies")
             sortedProduce.addAll(displayItems.filter { displayItem ->
@@ -42,48 +47,62 @@ class SeasonalAdapter(private val items: List<ProduceItem>) : RecyclerView.Adapt
         private const val VIEW_TYPE_ITEM = 1
     }
 
-
     override fun getItemViewType(position: Int): Int {
         if (itemsWithHeaders[position] is ProduceItem) {
             return VIEW_TYPE_ITEM
         }
-
         return VIEW_TYPE_HEADER
     }
 
+
+    inner class HeaderViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val headerLabel: TextView by bindView(R.id.tvHeader)
+    }
+
+    inner class BrowseItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        val name: TextView by bindView(R.id.tvName)
+        val type: TextView by bindView(R.id.tvType)
+        val img: ImageView by bindView(R.id.ivProduce)
+
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
 
         return when (viewType) {
             VIEW_TYPE_ITEM -> {
-                val view = BrowseItemView.inflate(parent)
-                SimpleViewHolder(view)
+                val v = parent.context.inflate(R.layout.item_produce, parent, false)
+                BrowseItemViewHolder(v)
             }
             else -> {
-                val headerView = HeaderItemView.inflate(parent)
-                SimpleViewHolder(headerView)
+                val v = parent.context.inflate(R.layout.item_header, parent, false)
+                HeaderViewHolder(v)
             }
         }
-
-
     }
-
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         when (holder.itemViewType) {
             VIEW_TYPE_ITEM -> {
-                val itemHolder = holder as SimpleViewHolder<BrowseItemView>
-                itemHolder.view.produce = itemsWithHeaders[position] as ProduceItem
+                val itemHolder = holder as BrowseItemViewHolder
+                val item = itemsWithHeaders[position] as ProduceItem
+                itemHolder.name.text = item.name
+                itemHolder.type.text = item.type
+
+                Glide.with(holder.itemView)
+                        .load(item.imgURL)
+                        .into(itemHolder.img)
+
             }
             VIEW_TYPE_HEADER -> {
-                val headerHolder = holder as SimpleViewHolder<HeaderItemView>
-                headerHolder.view.header = itemsWithHeaders[position].toString()
+
+                val headerViewHolder = holder as HeaderViewHolder
+                val sectionTitle = itemsWithHeaders[position] as String
+                headerViewHolder.headerLabel.text = sectionTitle
             }
 
         }
     }
 
     override fun getItemCount(): Int = itemsWithHeaders.size
-
-
 }
--- app/src/main/java/com/example/seetha/seasonalclone/util/ContextExt.kt
+++ app/src/main/java/com/example/seetha/seasonalclone/util/ContextExt.kt
@@ -6,14 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-
 // See also the KTX extensions such as .systemService()
-
-fun Context.inflater(): LayoutInflater = LayoutInflater.from(this)
 
 fun Context.inflate(@LayoutRes resource: Int, root: ViewGroup?, attachToRoot: Boolean): View =
         LayoutInflater.from(this).inflate(resource, root, attachToRoot)
-
-fun Context.inflate(@LayoutRes resource: Int, root: ViewGroup?): View =
-        LayoutInflater.from(this).inflate(resource, root)
-
